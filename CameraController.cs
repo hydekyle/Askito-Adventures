e042afeebcf5e480ad840b1bccc3fbe62f2200e4
Hyde_Wick <hydekyle@hotmail.com>
2020-06-25 22:38:16 +0100

    working cosas

--- CameraController.cs
+++ CameraController.cs
@@ -13,6 +13,8 @@ public class CameraController : MonoBehaviour
     Camera mainCamera;
     int cameraSize = 27;
     float verticalExtra = 0;
+    public Material backgroundMaterial;
+    public Material backgroundFarMaterial;
 
     private void Awake()
     {
@@ -22,22 +24,16 @@ public class CameraController : MonoBehaviour
 
     void FollowTarget()
     {
-        // float yAxis = Input.GetAxis("Vertical");
-        // if (Mathf.Abs(yAxis) > 0.3f)
-        // {
-        //     verticalExtra = Mathf.Clamp(Mathf.MoveTowards(verticalExtra, verticalExtra + yAxis * 4, Time.deltaTime * velocity * 9), -7f, 7f);
-        // }
-        // else
-        // {
-        //     verticalExtra = Mathf.MoveTowards(verticalExtra, 0, Time.deltaTime * velocity * 3);
-        // }
-        //mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, cameraSize, Time.deltaTime * velocity);
         target = playerT.position;
         transform.position = new Vector3(
             Mathf.Clamp(Mathf.Lerp(transform.position.x, target.x, Time.deltaTime * velocity), 0, 54f),
             transform.position.y,
             transform.position.z
         );
+        Vector2 backgroundOffset = backgroundMaterial.mainTextureOffset;
+        Vector2 backgroundFarOffset = backgroundFarMaterial.mainTextureOffset;
+        backgroundMaterial.SetTextureOffset("_MainTex", new Vector2(transform.position.x / 200f, 0));
+        backgroundFarMaterial.SetTextureOffset("_MainTex", new Vector2(transform.position.x / 600f, 0));
     }
 
     private void Update()
@@ -49,9 +45,15 @@ public class CameraController : MonoBehaviour
     {
         switch (zoomMode)
         {
-            case ZoomOptions.Focus: cameraSize = 18; break;
-            case ZoomOptions.Far: cameraSize = 30; break;
-            default: cameraSize = 27; break;
+            case ZoomOptions.Focus:
+                cameraSize = 18;
+                break;
+            case ZoomOptions.Far:
+                cameraSize = 30;
+                break;
+            default:
+                cameraSize = 27;
+                break;
         }
     }
 
